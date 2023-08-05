31e5914752409be02a1b5a39f234aef3ffbd791e
Aesha Shibeeb <aeshashibeeb@gmail.com>
2023-08-06 01:50:47 +0300

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-let loginBtn = document.getElementById("login")
-let usernameEl = document.getElementById("username")
-let passwordEl = document.getElementById("password")
+
+const loginBtn = document.getElementById("login")
+const usernameEl = document.getElementById("username")
+const passwordEl = document.getElementById("password")
 document.title = ("login Page")
 
 loginBtn.addEventListener("click", function(){
     // حطينا الفاليو كرمال يعطيني اللي كتبنا بقلبن بدال التاغ تبعن
     let accessUser = usernameEl.value 
     let accessPass = passwordEl.value
-    console.log(accessUser)
-    console.log(accessPass)
+    // console.log(accessUser, accessPass)
+    // if(accessUser == "admin01" && accessPass == "123"){
+
+    // }else if (accessUser == "admin01" && accessPass == "123")
 })
